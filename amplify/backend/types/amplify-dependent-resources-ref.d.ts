2ea3fd7f19e161fcb614d0d4188dee438592bf93
Michael Corpron <corpron@amazon.com>
2022-04-29 14:43:29 -0700

    authentication

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "amplifyapp08694c72": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { Amplify } from 'aws-amplify';
 
-function App() {
+import { withAuthenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
+
+import awsExports from './aws-exports';
+Amplify.configure(awsExports);
+
+function App({ signOut, user }) {
   return (
     <div className="App" style={{
       backgroundColor: 'blue',
       width: '100vw',
       height: '100vh'
     }}>
-
+      <>
+        <h1>Hello {user.username}</h1>
+        <button onClick={signOut}>Sign out</button>
+      </>
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
